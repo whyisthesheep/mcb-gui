c746b6cb5ff74b9c78e33d29582ae8d22cea6b2f
Leon J <leonjagger@icloud.com>
2023-12-29 14:03:00 +0000

    Added install script

---
+++ install.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Downloading Minecraft Bedrock Server...
+curl -o bedrock-server.zip https://minecraft.azureedge.net/bin-win/bedrock-server-1.20.51.01.zip
+
+echo Creating server folder...
+mkdir server
+
+echo Extracting files to server folder...
+tar -xf bedrock-server.zip -C server
+
+echo Cleaning up...
+del bedrock-server.zip
+
+echo Minecraft Bedrock Server installation complete.
+npm install
+pause
